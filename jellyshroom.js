f38441b9332448efa7c20826719ccd2c92f6b60b
David Sasson <david.e.sasson@gmail.com>
2019-04-27 14:02:47 -0700

    added jellyshrooms class

---
+++ jellyshroom.js
@@ -0,0 +1,22 @@
+Class Jellyshroom {
+
+  var body;
+  var feet = [];
+
+  var x, y;
+
+  var state = 0
+
+  function Constructor(xLoc, yLoc) {
+
+  }
+
+  function draw() {
+
+  }
+
+  function transition() {
+
+  }
+
+}
